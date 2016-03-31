3f1297c51c809408777b8a12117569e7500e3197
Fernando Barbeiro Campos <fernandoabcampos@gmail.com>
2016-03-31 20:30:53 +0200

    Renaming the edge server to be equal to the blog post

--- edge-server/src/main/java/com/spring/netflix/oss/microservices/EdgeServerApplication.java
+++ edge-server/src/main/java/com/spring/netflix/oss/microservices/EdgeServerApplication.java
@@ -3,14 +3,14 @@ package com.spring.netflix.oss.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
-@EnableZuulProxy
 @EnableDiscoveryClient
-public class EdgeProxyServerApplication {
+@EnableZuulServer
+public class EdgeServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EdgeProxyServerApplication.class, args);
+		SpringApplication.run(EdgeServerApplication.class, args);
 	}
 }
--- edge-server/src/test/java/com/spring/netflix/oss/microservices/EdgeServerApplicationTests.java
+++ edge-server/src/test/java/com/spring/netflix/oss/microservices/EdgeServerApplicationTests.java
@@ -2,14 +2,12 @@ package com.spring.netflix.oss.microservices;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = EdgeProxyServerApplication.class)
-@WebAppConfiguration
-public class EdgeProxyServerApplicationTests {
+@SpringApplicationConfiguration(classes = EdgeServerApplication.class)
+public class EdgeServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
