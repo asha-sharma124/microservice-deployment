186b4341968e354613dc537f52c424f47078067e
Fernando Barbeiro Campos <fernandoabcampos@gmail.com>
2016-04-02 18:43:34 +0200

    Creating a module for card-statements.

---
+++ statement-service/src/main/java/com/spring/netflix/oss/microservices/StatementServiceApplication.java
@@ -0,0 +1,12 @@
+package com.spring.netflix.oss.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatementServiceApplication.class, args);
+	}
+}
