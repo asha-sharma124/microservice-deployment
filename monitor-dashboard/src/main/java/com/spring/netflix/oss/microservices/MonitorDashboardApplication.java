10b70b1b3a0e194ada3d9b7648537eece39a1c6a
Fernando Barbeiro Campos <fernandoabcampos@gmail.com>
2016-04-27 22:37:04 +0200

    Enabling eureka for module monitor dashboard.

--- monitor-dashboard/src/main/java/com/spring/netflix/oss/microservices/MonitorDashboardApplication.java
+++ monitor-dashboard/src/main/java/com/spring/netflix/oss/microservices/MonitorDashboardApplication.java
@@ -2,12 +2,14 @@ package com.spring.netflix.oss.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableDiscoveryClient
 @Controller
 public class MonitorDashboardApplication {
 
