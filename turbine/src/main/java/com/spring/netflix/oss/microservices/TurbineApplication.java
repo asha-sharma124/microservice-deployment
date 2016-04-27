85dd9f52f5c29bf38686bda213758fcb3f385975
Fernando Barbeiro Campos <fernandoabcampos@gmail.com>
2016-04-27 22:46:01 +0200

    Enabling the correct dependency and arranging the code to add the annotations.

--- turbine/src/main/java/com/spring/netflix/oss/microservices/TurbineApplication.java
+++ turbine/src/main/java/com/spring/netflix/oss/microservices/TurbineApplication.java
@@ -2,8 +2,12 @@ package com.spring.netflix.oss.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.amqp.EnableTurbineAmqp;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableTurbineAmqp
 public class TurbineApplication {
 
 	public static void main(String[] args) {
