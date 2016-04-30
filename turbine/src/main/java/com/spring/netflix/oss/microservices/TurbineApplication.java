5097278c018c7dda9d9765e377fee18a31aa5a9e
Fernando Barbeiro Campos <fernandoabcampos@gmail.com>
2016-04-30 21:47:06 +0200

    Changing the pom to adjust the dependencies. Reorganizing project

--- turbine/src/main/java/com/spring/netflix/oss/microservices/TurbineApplication.java
+++ turbine/src/main/java/com/spring/netflix/oss/microservices/TurbineApplication.java
@@ -1,16 +1,16 @@
 package com.spring.netflix.oss.microservices;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.amqp.EnableTurbineAmqp;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableTurbineAmqp
+@EnableDiscoveryClient
 public class TurbineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TurbineApplication.class, args);
+		new SpringApplicationBuilder(TurbineApplication.class).run(args);
 	}
 }
